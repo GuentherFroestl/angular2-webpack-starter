9d8be9c3439dde70b1bbcb17f6e3d01b75fbd79b
Günther Fröstl <gfr@gammadata.de>
2016-09-27 20:56:54 +0300

    first sample app completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<div><h1>Hello World, this is my first ng2-app</h1></div>'
+})
+export class AppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
